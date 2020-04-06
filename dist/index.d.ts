11900cb971fdd4702cf28e1e1cf210a49d981d6a
Juho Vepsäläinen <bebraw@gmail.com>
2020-04-06 11:31:56 +0300

    chore: Add initial build

---
+++ dist/index.d.ts
@@ -0,0 +1,79 @@
+/// <reference types="webpack-dev-server" />
+import webpack from "webpack";
+import merge from "webpack-merge";
+declare function mergeConfig(): merge.ConfigurationMergeFunction;
+declare function mergeStorybook({
+  mode,
+  config,
+  userConfig,
+}: {
+  mode: "DEVELOPMENT" | "PRODUCTION";
+  config: webpack.Configuration;
+  userConfig: webpack.Configuration;
+}): webpack.Configuration;
+declare function loadJavaScript({
+  include,
+}: {
+  include: webpack.RuleSetCondition;
+}): webpack.Configuration;
+declare function loadTypeScript(): webpack.Configuration;
+declare function loadJSON(): webpack.Configuration;
+declare function loadLess(): webpack.Configuration;
+declare function loadCSS(): webpack.Configuration;
+declare function loadFonts(): webpack.Configuration;
+declare function loadImages(): webpack.Configuration;
+declare function dontParse(
+  paths: webpack.Module["noParse"],
+): webpack.Configuration;
+declare function webpackDevServer({
+  https,
+  staticPaths,
+}?: {
+  https: undefined;
+  staticPaths: string;
+}): webpack.Configuration;
+declare function webpackPluginServe({
+  https,
+  staticPaths,
+}?: {
+  https: undefined;
+  staticPaths: string;
+}): webpack.Configuration;
+declare function trackBundleSize(token: string): webpack.Configuration;
+declare function minifyJavaScript(): webpack.Configuration;
+declare function minifyCSS(): webpack.Configuration;
+declare function cleanOutput(): webpack.Configuration;
+declare function emitStats({
+  path,
+  filename,
+  publicPath,
+  logTime,
+}: {
+  path: string;
+  filename: string;
+  publicPath: string;
+  logTime: boolean;
+}): webpack.Configuration;
+declare function provideGlobals(globals: {
+  [key: string]: any;
+}): webpack.Configuration;
+export {
+  mergeConfig,
+  mergeStorybook,
+  loadJavaScript,
+  loadTypeScript,
+  loadJSON,
+  loadLess,
+  loadCSS,
+  loadFonts,
+  loadImages,
+  dontParse,
+  webpackDevServer,
+  webpackPluginServe,
+  trackBundleSize,
+  minifyJavaScript,
+  minifyCSS,
+  cleanOutput,
+  emitStats,
+  provideGlobals,
+};
