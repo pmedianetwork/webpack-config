77d9e23162795ceb92259d8501f9035939648e02
Juho Vepsäläinen <bebraw@gmail.com>
2020-04-28 10:43:05 +0300

    feat(loadImages): For SVGs processed through jsx/tsx, apply svgr to them
    Doing this will export the SVGs as React components to be directly
    consumed.

--- src/index.ts
+++ src/index.ts
@@ -330,6 +330,15 @@ function loadImages(options: FileLoaderOptions = {}): webpack.Configuration {
             options: { limit: 15000, name: "[name].[ext]", ...options },
           },
         },
+        // For svg loaded from jsx/tsx, process it as a React component
+        // More info: https://www.npmjs.com/package/@svgr/webpack
+        {
+          test: /\.(svg)$/,
+          issuer: {
+            test: /\.(jsx|tsx)$/,
+          },
+          use: ["@svgr/webpack", "file-loader"],
+        },
         {
           test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
           use: {
