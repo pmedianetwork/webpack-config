bd22545afc9184d0b6428ea08026608402eb6eb9
Juho Vepsäläinen <bebraw@gmail.com>
2020-07-13 12:44:24 +0200

    fix: Add a more robust method for figuring out app path

--- utils/copy-browserslist.js
+++ utils/copy-browserslist.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
+// https://stackoverflow.com/questions/10265798/determine-project-root-from-a-running-node-js-application
+const { path: appRootPath } = require("app-root-path");
+
 function copyBrowserslist() {
   const packageDirectory = path.join(__dirname, "..");
   const browsersListPath = path.join(packageDirectory, ".browserslistrc");
 
-  // This assumes npm install was run within the project root directory.
-  const targetPath = process.cwd();
+  console.log(`Copying ${browsersListPath} to ${appRootPath}`);
 
   try {
-    fs.copyFileSync(browsersListPath, targetPath);
+    fs.copyFileSync(browsersListPath, appRootPath);
   } catch (err) {
     console.error(err);
   }
