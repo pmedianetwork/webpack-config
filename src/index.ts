4dbfec79e563875f10141ba4d89196d78f332ac6
Juho Vepsäläinen <bebraw@gmail.com>
2020-10-21 12:42:10 +0200

    fix: Fix type issue with webpack-plugin-serve
    It looks like webpack-plugin-serve types need a bigger update.
    
    Closes #45.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import webpack from "webpack";
 import {
   WebpackPluginServe,
   WebpackPluginServeOptions,
+  // @ts-ignore: webpack-plugin-serve types are broken and need an update
 } from "webpack-plugin-serve";
 import merge from "webpack-merge";
 import BrotliPlugin from "brotli-webpack-plugin";
@@ -432,7 +433,9 @@ function webpackPluginServe({
         hmr: "refresh-on-failure",
         progress: "minimal",
         historyFallback,
+        // @ts-ignore: webpack-plugin-serve types are broken
         middleware: (app) =>
+          // @ts-ignore: Same here
           app.use(async (ctx, next) => {
             ctx.set("Access-Control-Allow-Origin", "*");
             await next();
